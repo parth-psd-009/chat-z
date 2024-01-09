48fec288b6a5a7519f4c1b6cfc389b281f83966f
Parth Sarathi Dixit <dixitparthsarathi@gmail.com>
2024-01-09 10:31:17 +0530

    Some changes

--- frontend/src/components/Navigation/Navigation.component.jsx
+++ frontend/src/components/Navigation/Navigation.component.jsx
@@ -1,7 +1,14 @@
+import "./Navigation.styles.scss";
 const Navigation = () => {
     return (
-        <div>
-            <h2>Navigation</h2>
+        <div className="nav-container">
+            <div className="nav-logo">Navigation</div>
+            <ul className="nav-links">
+                <li className="nav-link-item">Home</li>
+                <li className="nav-link-item">SignUp</li>
+                <li className="nav-link-item">Explore</li>
+                <li className="nav-link-item">Contact</li>
+            </ul>
         </div>
     );
 };
